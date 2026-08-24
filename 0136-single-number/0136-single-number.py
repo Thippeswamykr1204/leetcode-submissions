deb2971c8eab17b9c79d059adc73eb546e63ced0
Thippeswamy K R <kollithippeswamy1204@gmail.com>
2026-08-24 14:54:38 +0530

    Time: 3 ms (60.64%), Space: 21.1 MB (43.71%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,10 +1,17 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        freq = {}
+        # By using arrays
+        # freq = {}      
 
+        # for i in nums:
+        #     freq[i] = freq.get(i, 0) + 1
+
+        # for i in freq:
+        #     if freq[i] == 1:
+        #         return i
+
+        # By using Bit Manipulations
+        result = 0
         for i in nums:
-            freq[i] = freq.get(i, 0) + 1
-
-        for i in freq:
-            if freq[i] == 1:
-                return i
+            result ^= i 
+        return result
